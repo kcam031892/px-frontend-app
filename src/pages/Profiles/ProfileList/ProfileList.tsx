858002cced050878eae6253300e9ce71a0ccc800
Mhack Bautista <mack031892@gmail.com>
2022-03-26 21:12:11 +0800

    added alert component and convert error response to array of string

--- src/pages/Profiles/ProfileList/ProfileList.tsx
+++ src/pages/Profiles/ProfileList/ProfileList.tsx
@@ -4,14 +4,15 @@ import { Box, Grid, Typography } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import NewProfileDialog from './NewProfileDialog/NewProfileDialog';
 import ProfileListItem from './ProfileListItem/ProfileListItem';
-import { Button } from 'themes/elements';
+import { Button, useAlert } from 'themes/elements';
 import { profileService } from 'shared/services/profileService';
 import { RepresentationType } from 'shared/enums/RepresentationType';
 
 const { getProfiles } = profileService();
 const ProfileList = () => {
   const { data } = getProfiles();
-  const profileList = Array.from({ length: 12 }).fill('');
+  const { isOpen: isAlertOpen, alertRef, AlertOpen } = useAlert({ autoHideDuration: 2000, horizontal: 'right' });
+
   const classes = useStyles();
   const [isProfileDialogOpen, setIsProfileDialogOpen] = useState<boolean>(false);
   const handleOpenProfileDialog = () => setIsProfileDialogOpen(true);
@@ -33,14 +34,17 @@ const ProfileList = () => {
           data.data.length > 0 &&
           data.data.map((profile, index) => <ProfileListItem profile={profile} key={index} />)}
       </Grid>
+
       {data && (
         <NewProfileDialog
           open={isProfileDialogOpen}
           onClose={handleCloseProfileDialog}
           hasFreelance={hasFreelance}
           profiles={data.data}
+          AlertOpen={AlertOpen}
         />
       )}
+      {isAlertOpen && alertRef}
     </Box>
   );
 };
