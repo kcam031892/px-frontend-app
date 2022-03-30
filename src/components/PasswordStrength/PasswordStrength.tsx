dc31fae438de2776ed027ad8339a2c7fb5815975
R-Jill Ham R. Manuel <rjillham.manuel@gmail.com>
2022-03-30 16:31:43 +0800

    added space to password strength

--- src/components/PasswordStrength/PasswordStrength.tsx
+++ src/components/PasswordStrength/PasswordStrength.tsx
@@ -5,6 +5,7 @@ const PasswordStrength = () => {
   return (
     <Fragment>
       <Typography variant="subtitle2">Your password must be at least 8 characters in length and include:</Typography>
+      <br />
       <Typography variant="body2">• &nbsp;a minimum of 1 lower case letter [a-z] and</Typography>
       <Typography variant="body2">• &nbsp;a minimum of 1 upper case letter [A-Z] and</Typography>
       <Typography variant="body2">• &nbsp;a minimum of 1 numeric character [0-9] and</Typography>
