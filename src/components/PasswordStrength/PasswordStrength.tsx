e63c82002c7cae4d52d4f7bf9a8768d9b11251bd
R-Jill Ham R. Manuel <rjillham.manuel@gmail.com>
2022-03-30 14:33:19 +0800

    re-added passwordStrength component

---
+++ src/components/PasswordStrength/PasswordStrength.tsx
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+import { Typography } from '@material-ui/core';
+
+const PasswordStrength = () => {
+  return (
+    <Fragment>
+      <Typography variant="subtitle2">
+        Your password must be at least 8 characters in length and include:
+      </Typography>
+      <Typography variant="body2">• &nbsp;a minimum of 1 lower case letter [a-z] and</Typography>
+      <Typography variant="body2">• &nbsp;a minimum of 1 upper case letter [A-Z] and</Typography>
+      <Typography variant="body2">• &nbsp;a minimum of 1 numeric character [0-9] and</Typography>
+      <Typography variant="body2">
+        • &nbsp;a minimum of 1 special character: &nbsp;&nbsp;
+        {'~`!@#$%^&*()-_+={}[]|;:"<>,./?'}
+      </Typography>
+    </Fragment>
+  );
+};
+
+export default PasswordStrength;
